8ae8e87ae0ee55b258aa276739e5ee3cd49d35d3
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-18 21:15:03 -0300

    feat: get artists from spotify API

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -1,34 +1,67 @@
+import { useEffect, useState } from 'react'
+
 export default function Artists() {
+    const clientId = import.meta.env.VITE_CLIENT_ID
+    const clientSecret = import.meta.env.VITE_CLIENT_SECRET
+
+    const [accessToken, setAccessToken] = useState('')
+    const [artists, setArtists] = useState([])
+
+    useEffect(() => {
+        const authHeader = btoa(`${clientId}:${clientSecret}`)
+
+        fetch('https://accounts.spotify.com/api/token', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': `Basic ${authHeader}`
+            },
+            body: `grant_type=client_credentials`
+        })
+            .then(response => response.json())
+            .then(data => setAccessToken(data.access_token))
+    }, [])
+
+    useEffect(() => {
+        if (!accessToken) return
+
+        const fetchArtists = async () => {
+            const response = await fetch(
+                'https://api.spotify.com/v1/search?q=rock&type=artist&limit=10',
+                {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                }
+            )
+
+            const data = await response.json()
+            setArtists(data.artists.items)
+        }
+
+        fetchArtists()
+    }, [accessToken])
+
     return  (
         <article>
             <h1 className="text-2xl font-medium">
                 Artists recommended for you
             </h1>
             <section className="flex mt-4 gap-6">
-                <div>
-                    <figure className="bg-gray-400 h-36 w-36 rounded-md">
-                        img
-                    </figure>
-                    <figcaption className="mt-2 text-gray-300 text-sm">
-                        Artist Name
-                    </figcaption>
-                </div>
-                <div>
-                    <figure className="bg-gray-400 h-36 w-36 rounded-md">
-                        img
-                    </figure>
-                    <figcaption className="mt-2 text-gray-300 text-sm">
-                        Artist Name
-                    </figcaption>
-                </div>
-                <div>
-                    <figure className="bg-gray-400 h-36 w-36 rounded-md">
-                        img
-                    </figure>
-                    <figcaption className="mt-2 text-gray-300 text-sm">
-                        Artist Name
-                    </figcaption>
-                </div>  
+                {artists.map(artist => (
+                    <div>
+                        <figure className="bg-gray-400 h-48 w-48 rounded-md">
+                            <img
+                                src={artist.images[0].url}
+                                alt={artist.name}
+                                className="h-48 w-48 object-cover"
+                            />
+                        </figure>
+                        <figcaption className="mt-2 text-gray-300 text-sm">
+                            {artist.name}
+                        </figcaption>
+                    </div>
+                ))}
             </section>
         </article>
     )
