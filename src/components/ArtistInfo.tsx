72d1532c023dd3a6cef397887fc1e4a855f84c0d
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-19 18:25:36 -0300

    wip: ArtistInfo component and links structure

---
+++ src/components/ArtistInfo.tsx
@@ -0,0 +1,5 @@
+export default function ArtistInfo() {
+    return (
+        <h1>teste</h1>
+    )
+}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,15 @@
 import Artists from './Artists'
+import ArtistInfo from './ArtistInfo'
+import { Routes, Route } from 'react-router-dom'
 
 export default function Main() {
     return (
         <main className="bg-gray-800 text-gray-100 p-5 min-h-max">
             <article>
-                <Artists />
+                <Routes>
+                    <Route path="/" element={<Artists />} />
+                    <Route path="/artists/:query" element={<ArtistInfo/>} />
+                </Routes>
             </article>
         </main>
     )
