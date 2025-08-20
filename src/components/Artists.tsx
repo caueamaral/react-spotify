9c867233c2f52a60aaf81527d6bd46bbb3cd1291
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-20 18:12:02 -0300

    refactor(artists): adjust HTML semantics for artist list

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -25,9 +25,9 @@ export default function Artists() {
             <h1 className="text-2xl font-medium">
                 Artists in: Rock
             </h1>
-            <section className="grid gap-5 mt-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-8">
+            <div className="grid gap-5 mt-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-8">
                 {artists.map(artist => (
-                    <div key={artist.id} className="relative">
+                    <section key={artist.id} className="relative">
                         <figure className="bg-gray-400 w-full aspect-square rounded-md overflow-hidden">
                             <img
                                 src={artist.images[1]?.url}
@@ -39,9 +39,9 @@ export default function Artists() {
                             {artist.name}
                         </figcaption>
                         <Link to={`/artist/${artist.id}/${formatText(artist.name)}`} className="absolute inset-0"></Link>
-                    </div>
+                    </section>
                 ))}
-            </section>
+            </div>
         </article>
     )
 }
