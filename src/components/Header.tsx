454457fc714c269bab7927d409dfffbcb7218169
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-22 13:31:58 -0300

    refactor: replace plain CSS with Tailwind classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Search from './Search'
 
 export default function Header() {
     return  (
-        <header className="flex items-center justify-between p-5">
+        <header className="bg-black flex items-center justify-between p-5">
             <Logo />
             <Search />
         </header>
