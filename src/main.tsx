bc4d1c18805ae318e922d22a94bdef1031d67c7e
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-22 09:32:26 -0300

    refactor(css): move styles into dedicated folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { AccessTokenProvider } from './contexts/AccessTokenContext'
-import './index.css'
+import './css/tailwind.css'
+import './css/styles.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
