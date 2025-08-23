c3b66c264ce8f75a86aa44a34ce28f38037e0e79
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-22 22:53:16 -0300

    fix(album): return single Album instead of Album[]

--- src/services/getAlbum.ts
+++ src/services/getAlbum.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type Album from '../interfaces/Album'
 
-export default async function getAlbum(accessToken: string, artistId: string): Promise<Album[]> {
+export default async function getAlbum(accessToken: string, artistId: string): Promise<Album> {
     const response = await axios.get(
         `https://api.spotify.com/v1/albums/${artistId}`,
         {
