95fbee16a940d805ff58a72ff9d92fbe11fe7ef9
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-24 01:56:28 -0300

    fix(ci): add base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+    base: '/react-spotify/',
     plugins: [
         tailwindcss()
     ],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/setupTests.ts',
-    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}']
-  },
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './src/setupTests.ts',
+      include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}']
+    },
 })
