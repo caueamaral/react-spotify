db5677aab187e5e5a7502431700942cd75675ea6
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-24 17:29:45 -0300

    refactor(search): improve loading state

--- src/components/SearchInfo.tsx
+++ src/components/SearchInfo.tsx
@@ -11,49 +11,49 @@ import type SearchResult from '../interfaces/SearchResult'
 export default function SearchInfo() {
     const accessToken = useAccessToken()
     const [searchResult, setSearchResult] = useState<SearchResult | null>(null)
+    const [isLoading, setIsLoading] = useState(true)
     const { query } = useParams<{ query: string }>()
 
     useEffect(() => {
         if (!accessToken || !query) return
 
         getSearch(accessToken, query)
-            .then(response => setSearchResult(response))
+            .then(setSearchResult)
+            .finally(() => setIsLoading(false))
+
     }, [accessToken, query])
+
+    if (isLoading) {
+        return <Title text="Searching..." />
+    }
 
     return (
         <>
-            {
-                !searchResult || !searchResult.artists ? (
-                    <Title text="Loading artists results..." />
-                ) : (
-                    <article>
-                        <Title text={`Artist found`} />
-                        <div className="grid gap-5 mt-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-8">
-                            <ArtistCard
-                                key={searchResult.artists.items[0].id}
-                                artist={searchResult.artists.items[0]}
-                                index={1}
-                            />
-                        </div>
-                    </article>
-                )
-            }
-            {
-                !searchResult || !searchResult.albums ? (
-                    <Title text="Loading tracks results..." />
-                ) : (
-                    <article className="mt-10">
-                        <Title text={`Album found`} />
-                        <div className="grid gap-5 mt-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-8">
-                            <AlbumCard
-                                key={searchResult.albums.items[0].id}
-                                album={searchResult.albums.items[0]}
-                                index={1}
-                            />
-                        </div>
-                    </article>
-                )
-            }
+            {searchResult?.artists?.items.length ? (
+                <article>
+                    <Title text={`Artist found`} />
+                    <div className="grid gap-5 mt-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-8">
+                        <ArtistCard
+                            key={searchResult.artists.items[0].id}
+                            artist={searchResult.artists.items[0]}
+                            index={1}
+                        />
+                    </div>
+                </article>
+            ) : null}
+
+            {searchResult?.albums?.items.length ? (
+                <article className="mt-10">
+                    <Title text={`Album found`} />
+                    <div className="grid gap-5 mt-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-8">
+                        <AlbumCard
+                            key={searchResult.albums.items[0].id}
+                            album={searchResult.albums.items[0]}
+                            index={1}
+                        />
+                    </div>
+                </article>
+            ) : null}
         </>
     )
 }
