c8751987490343a9c3c798b5d50961ff5a1f9dea
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-25 09:37:26 -0300

    feat(artists): add React Query for caching artists

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import { useAccessToken } from '../contexts/AccessTokenContext'
 
 import type Artist from '../interfaces/Artist'
@@ -9,28 +9,34 @@ import ArtistCard from '../components/ArtistCard'
 
 export default function Artists() {
     const accessToken = useAccessToken()
-    const [artistsRock, setArtistsRock] = useState<Artist[]>([])
-    const [artistsPop, setArtistsPop] = useState<Artist[]>([])
-    const [artistsCountry, setArtistsCountry] = useState<Artist[]>([])
-    const [isLoading, setIsLoading] = useState(true)
+    const fiveMinutes = 1000 * 60 * 5
 
-    useEffect(() => {
-        if (!accessToken) return
+    const { data: artistsRock, isLoading: loadingRock } = useQuery<Artist[]>({
+        queryKey: ['artists', 'rock'],
+        queryFn: () => getArtists(accessToken!, 'rock'),
+        enabled: !!accessToken,
+        staleTime: fiveMinutes
+    })
 
-        Promise.all([
-            getArtists(accessToken, 'rock').then(setArtistsRock),
-            getArtists(accessToken, 'pop').then(setArtistsPop),
-            getArtists(accessToken, 'country').then(setArtistsCountry)
-        ])
-            .finally(() => setIsLoading(false))
+    const { data: artistsPop, isLoading: loadingPop } = useQuery<Artist[]>({
+        queryKey: ['artists', 'pop'],
+        queryFn: () => getArtists(accessToken!, 'pop'),
+        enabled: !!accessToken,
+        staleTime: fiveMinutes
+    })
 
-    }, [accessToken])
+    const { data: artistsCountry, isLoading } = useQuery<Artist[]>({
+        queryKey: ['artists', 'country'],
+        queryFn: () => getArtists(accessToken!, 'country'),
+        enabled: !!accessToken,
+        staleTime: fiveMinutes
+    })
 
-    const renderArtistsSection = (title: string, artists: Artist[]) => (
+    const renderArtistsSection = (title: string, artists?: Artist[]) => (
         <article>
             <Title text={title} />
             <div className="grid gap-5 mt-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-8">
-                {artists.map((artist, index) => (
+                {artists?.map((artist, index) => (
                     <ArtistCard
                         key={artist.id}
                         artist={artist}
@@ -41,7 +47,7 @@ export default function Artists() {
         </article>
     )
 
-    if (isLoading) {
+    if (loadingRock || loadingPop) {
         return <Title text="Loading Artists..." />
     }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { AccessTokenProvider } from './contexts/AccessTokenContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import './css/tailwind.css'
 import './css/styles.css'
 import App from './App.tsx'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AccessTokenProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <QueryClientProvider client={queryClient}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </QueryClientProvider>
     </AccessTokenProvider>
   </StrictMode>,
 )
